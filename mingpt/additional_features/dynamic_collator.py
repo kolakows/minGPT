e790f2e412b901cd1de30346330736d8d6c0baf0
Igor <igor.kolakowski@websensa.com>
2022-05-16 08:50:35 +0200

    add truncation in collator

--- mingpt/additional_features/dynamic_collator.py
+++ mingpt/additional_features/dynamic_collator.py
@@ -3,9 +3,15 @@ from torch.nn.utils.rnn import pad_sequence
 
 class DynamicCollator:
 
+    def __init__(self, block_size):
+        self.block_size = block_size
+
     def __call__(self, examples):
-        inputs = [item[0] for item in examples]
-        targets = [item[1] for item in examples]
+        # truncate to block_size
+        inputs = [item[0][:self.block_size] for item in examples]
+        targets = [item[1][:self.block_size] for item in examples]
+
         inputs = pad_sequence(inputs, batch_first=True, padding_value=0)
         targets = pad_sequence(targets, batch_first=True, padding_value=-100)
+
         return inputs, targets
